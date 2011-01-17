b3fe9cdad795d078222fddb60c77ee29380d7f7c
Tomohiro Hashidate <t-hashidate@nri.co.jp>
2011-01-17 12:24:57 +0900

    add custom rake completion

---
+++ zsh/completion/rake_completion.zsh
@@ -0,0 +1,21 @@
+_rake_does_task_list_need_generating () {
+  if [ ! -f .rake_tasks ]; then return 0;
+  else
+    accurate=$(stat -c %Y .rake_tasks)
+    changed=$(stat -c %Y Rakefile)
+    return $(expr $accurate '>=' $changed)
+  fi
+}
+
+_rake () {
+  if [ -f Rakefile ]; then
+    if _rake_does_task_list_need_generating; then
+      echo "\nGenerating .rake_tasks..." > /dev/stderr
+      rake --silent --tasks | cut -d " " -f 2 > .rake_tasks
+    fi
+    compadd `cat .rake_tasks`
+  fi
+}
+
+compdef _rake rake
+
