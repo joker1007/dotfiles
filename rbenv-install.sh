0f40395f2f17d6367686cf7dfe58cf8154addd1f
joker1007 <kakyoin.hierophant@gmail.com>
2014-06-06 17:44:25 +0900

    Update rbenv-install.sh

--- rbenv-install.sh
+++ rbenv-install.sh
@@ -1,1 +1,7 @@
-git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+mkdir -p ~/.rbenv/plugins
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/amatsuda/gem-src.git ~/.rbenv/plugins/gem-src
+git clone https://github.com/rkh/rbenv-update.git ~/.rbenv/plugins/rbenv-update
+git clone https://github.com/sstephenson/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems
+cp ./default-gems ~/.rbenv/default-gems
