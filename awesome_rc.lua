5d39a1ca2ece3455452752b66d64a9b7b4fc66c8
joker1007 <kakyoin.hierophant@gmail.com>
2017-09-12 21:19:16 +0900

    Add autorun.sh invoke

--- awesome_rc.lua
+++ awesome_rc.lua
@@ -618,3 +618,8 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+
+if os.execute("test -f ~/.config/awesome/autorun.sh ") == 0 then
+  awful.spawn.with_shell("~/.config/awesome/autorun.sh")
+end
