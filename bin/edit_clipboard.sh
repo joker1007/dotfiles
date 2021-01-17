0389d736e4e4a496cbf4345b63b3d2469e94da45
joker1007 <kakyoin.hierophant@gmail.com>
2021-01-18 01:38:00 +0900

    Add clipboard_edit

---
+++ bin/edit_clipboard.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -eo pipefail
+
+tmpfile=$(mktemp -q /tmp/edit_clipboard.XXXXXX)
+ctime1=$(stat -c "%Y" $tmpfile)
+empty=
+
+usage() {
+  echo -e "Usage: $0 [-e] [-t TERMINAL_CMD]\n\t-e\t\topen empty buffer \n\t-t TERMINAL_CMD\topen with terminal" 1>&2
+  exit 1
+}
+
+while getopts t:eh OPT
+do
+  case $OPT in
+    e) empty=true
+      ;;
+    t) TERMINAL_CMD=$OPTARG
+      ;;
+    h) usage
+      ;;
+    ?) usage
+      ;;
+  esac
+done
+
+atexit() {
+  [[ -f ${tmpfile} ]] && rm -f ${tmpfile}
+}
+
+trap atexit EXIT
+
+if [[ -z "$empty" ]]; then
+  xclip -selection clipboard -o >$tmpfile
+fi
+
+eval "$TERMINAL_CMD $EDITOR $tmpfile"
+
+result=$(< $tmpfile)
+ctime2=$(stat -c "%Y" $tmpfile)
+
+if [[ -n "$result" ]] && [[ $ctime2 -gt $ctime1 ]]; then
+  echo -n $result | xclip -selection clipboard -i
+  echo -n $result | xclip -selection primary -i
+fi
