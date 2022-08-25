48694ca8032f24a2d23405106068705db28c6b20
joker1007 <kakyoin.hierophant@gmail.com>
2022-08-26 01:01:28 +0900

    Fix wrong mapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -42,8 +42,8 @@ vim.opt.timeoutlen=500
 -- swap ; and :
 vim.keymap.set('n', ';', ':')
 vim.keymap.set('n', ':', ';')
-vim.keymap.set('n', ';', ':')
-vim.keymap.set('n', ':', ';')
+vim.keymap.set('v', ';', ':')
+vim.keymap.set('v', ':', ';')
 
 -- Space prefix
 
