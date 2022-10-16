adb6e94a7bbd5187345f252b9ee872c7f2279ae6
joker1007 <kakyoin.hierophant@gmail.com>
2022-10-16 23:05:33 +0900

    Fix deprecation of cmp_nvim_lsp

--- nvim/lua/lsp_common.lua
+++ nvim/lua/lsp_common.lua
@@ -2,7 +2,6 @@ local M = {}
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 M.capabilities = capabilities
 
