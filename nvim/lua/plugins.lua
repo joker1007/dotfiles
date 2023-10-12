323ba69e2e443f83cf0d146d3327e94cd12ff386
joker1007 <kakyoin.hierophant@gmail.com>
2023-10-12 19:04:37 +0900

    Update indent-blankline.nvim config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -317,26 +317,31 @@ return packer.startup(function(use)
 
   use({
     "lukas-reineke/indent-blankline.nvim",
-    setup = function()
-      vim.cmd [[hi IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-      vim.cmd [[hi IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-      vim.cmd [[hi IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-      vim.cmd [[hi IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-      vim.cmd [[hi IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-      vim.cmd [[hi IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-    end,
     config = function()
-      require("indent_blankline").setup({
-        char_highlight_list = {
-          "IndentBlanklineIndent1",
-          "IndentBlanklineIndent2",
-          "IndentBlanklineIndent3",
-          "IndentBlanklineIndent4",
-          "IndentBlanklineIndent5",
-          "IndentBlanklineIndent6",
-        },
-        show_current_context = true,
-      })
+      local highlight = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowGreen",
+        "RainbowOrange",
+        "RainbowViolet",
+        "RainbowCyan",
+      }
+
+      local hooks = require "ibl.hooks"
+      -- create the highlight groups in the highlight setup hook, so they are reset
+      -- every time the colorscheme changes
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      end)
+
+      require("ibl").setup { indent = { highlight = highlight } }
     end,
   })
 
