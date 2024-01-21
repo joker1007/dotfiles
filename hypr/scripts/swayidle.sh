aafe80205198b6cf78fa918d35405aa606ed9847
joker1007 <kakyoin.hierophant@gmail.com>
2024-01-21 10:47:25 +0900

    update hypr configs

--- hypr/scripts/swayidle.sh
+++ hypr/scripts/swayidle.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 exec swayidle -w \
+  lock 'swaylock -eFfk -c 000000' \
   timeout 3600 'swaylock -eFfk -c 000000' \
-  timeout 7200 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' \
-  before-sleep 'swaylock -eFfk -c 000000'
+  timeout 4000 'hyprctl dispatch dpms off' \
+  resume 'hyprctl dispatch dpms on' \
+  before-sleep 'swaylock -eFfk -c 000000' \
 
 # exec swayidle -w \
 #   timeout 3600 'swaylock -eFfk -c 000000' \
