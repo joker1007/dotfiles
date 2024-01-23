8040ef6d3c8952a2e6af8c93673fac25180489a6
joker1007 <kakyoin.hierophant@gmail.com>
2024-01-23 23:02:22 +0900

    update hyprland config

--- hypr/scripts/swayidle.sh
+++ hypr/scripts/swayidle.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+SWAYLOCK="swaylock -eFfk -c 000000"
+
+# swaylock-effects
+# SWAYLOCK="swaylock -eFfk --screenshots --clock --indicator --effect-scale 0.5 --effect-blur 7x5 --effect-vignette 0.5:0.5 --fade-in 0.5"
+
 exec swayidle -w \
-  lock 'swaylock -eFfk -c 000000' \
-  timeout 3600 'swaylock -eFfk -c 000000' \
+  lock "${SWAYLOCK}" \
+  timeout 3600 "${SWAYLOCK}" \
   timeout 4000 'hyprctl dispatch dpms off' \
   resume 'hyprctl dispatch dpms on' \
-  before-sleep 'swaylock -eFfk -c 000000' \
-
-# exec swayidle -w \
-#   timeout 3600 'swaylock -eFfk -c 000000' \
-#   before-sleep 'swaylock -eFfk -c 000000'
+  before-sleep "${SWAYLOCK}" \
