22f72c22c9514b6c9b974d74580cce088de4481c
joker1007 <kakyoin.hierophant@gmail.com>
2024-01-25 19:29:08 +0900

    update nvim config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1335,11 +1335,25 @@ require("lazy").setup({
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
-    cmd = { "Neotree" },
-    keys = { "<leader>tt" },
     config = function()
       vim.keymap.set("n", "<leader>tt", "<cmd>Neotree toggle filesystem reveal<cr>")
       require("neo-tree").setup({
+        filesystem = {
+          filtered_items = {
+            hide_gitignored = false,
+            always_show = {
+              ".gitignore",
+              ".envrc",
+            },
+          },
+          hijack_netrw_behavior = "open_current",
+          use_libuv_file_watcher = true,
+          window = {
+            mappings = {
+              ["."] = "toggle_hidden",
+            }
+          }
+        },
         window = {
           mappings = {
             ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
@@ -1348,42 +1362,42 @@ require("lazy").setup({
       })
     end,
   },
-  -- {
-  --   "3rd/image.nvim",
-  --   build = "luarocks --local install magick",
-  --   config = function()
-  --     if not vim.g.neovide then
-  --       require("image").setup({
-  --         backend = "ueberzug",
-  --         integrations = {
-  --           markdown = {
-  --             enabled = true,
-  --             clear_in_insert_mode = false,
-  --             download_remote_images = true,
-  --             only_render_image_at_cursor = false,
-  --             filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-  --           },
-  --           neorg = {
-  --             enabled = true,
-  --             clear_in_insert_mode = false,
-  --             download_remote_images = true,
-  --             only_render_image_at_cursor = false,
-  --             filetypes = { "norg" },
-  --           },
-  --         },
-  --         max_width = nil,
-  --         max_height = nil,
-  --         max_width_window_percentage = nil,
-  --         max_height_window_percentage = 50,
-  --         window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-  --         window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-  --         editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
-  --         tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-  --         hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
-  --       })
-  --     end
-  --   end,
-  -- },
+  {
+    "3rd/image.nvim",
+    build = "luarocks --local install magick",
+    config = function()
+      if not vim.g.neovide then
+        require("image").setup({
+          backend = "ueberzug",
+          integrations = {
+            markdown = {
+              enabled = true,
+              clear_in_insert_mode = true,
+              download_remote_images = true,
+              only_render_image_at_cursor = true,
+              filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+            },
+            neorg = {
+              enabled = true,
+              clear_in_insert_mode = false,
+              download_remote_images = true,
+              only_render_image_at_cursor = false,
+              filetypes = { "norg" },
+            },
+          },
+          max_width = 480,
+          max_height = 160,
+          max_width_window_percentage = nil,
+          max_height_window_percentage = nil,
+          window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+          window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+          editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+          tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+          hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
+        })
+      end
+    end,
+  },
   {
     "axieax/urlview.nvim",
     cmd = { "UrlView" },
