cb33b0616a57796a2293a21c3f3dd73bda935fee
joker1007 <kakyoin.hierophant@gmail.com>
2024-03-03 15:46:49 +0900

    use selene instead of luacheck

--- nvim/init.lua
+++ nvim/init.lua
@@ -383,9 +383,7 @@ null_ls.setup({
         return utils.root_has_file({ ".rubocop.yml" })
       end,
     }),
-    null_ls.builtins.diagnostics.luacheck.with({
-      extra_args = { "--globals", "vim", "--globals", "awesome" },
-    }),
+    null_ls.builtins.diagnostics.selene,
     null_ls.builtins.diagnostics.yamllint,
     null_ls.builtins.diagnostics.commitlint.with({
       condition = function(utils)
