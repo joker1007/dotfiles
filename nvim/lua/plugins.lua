d6bdafa047dd370548625861212fb1a86cb537b3
joker1007 <kakyoin.hierophant@gmail.com>
2024-11-05 23:51:52 +0900

    update luasnip keymap

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -847,12 +847,12 @@ require("lazy").setup({
       vim.cmd [[
         " press <Tab> to expand or jump in a snippet. These can also be mapped separately
         " via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
-        imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+        imap <silent><expr> <C-L> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<C-L>'
         " -1 for jumping backwards.
-        inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
+        inoremap <silent> <C-S-L> <cmd>lua require'luasnip'.jump(-1)<Cr>
 
-        snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
-        snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
+        snoremap <silent> <C-L> <cmd>lua require('luasnip').jump(1)<Cr>
+        snoremap <silent> <C-S-L> <cmd>lua require('luasnip').jump(-1)<Cr>
       ]]
     end,
     event = "InsertEnter",
