769636834f484a0c25e90054cf19caaa0d2e7036
joker1007 <kakyoin.hierophant@gmail.com>
2025-02-13 20:54:47 +0900

    update ruby-lsp config

--- nvim/lua/configs/enable_ruby_lsp.lua
+++ nvim/lua/configs/enable_ruby_lsp.lua
@@ -2,26 +2,26 @@ local lsp_common = require "lsp_common"
 local on_attach = lsp_common.on_attach
 local lspconfig = require "lspconfig"
 
-lspconfig.ruby_ls.setup({
-  capabilities = capabilities,
+lspconfig.ruby_lsp.setup({
+  capabilities = lsp_common.make_lsp_capabilities(),
   on_attach = on_attach,
-  init_options = {
-    enabledFeatures = {
-      "documentSymbol",
-      "documentLink",
-      "hover",
-      "foldingRanges",
-      "selectionRanges",
-      "semanticHighlighting",
-      "formatting",
-      "onTypeFormatting",
-      "diagnostics",
-      "codeActions",
-      "codeActionResolve",
-      "documentHighlight",
-      "inlayHints",
-      "completion",
-      "codeLens",
-    },
-  },
+  -- init_options = {
+  --   enabledFeatures = {
+  --     "documentSymbol",
+  --     "documentLink",
+  --     "hover",
+  --     "foldingRanges",
+  --     "selectionRanges",
+  --     "semanticHighlighting",
+  --     "formatting",
+  --     "onTypeFormatting",
+  --     "diagnostics",
+  --     "codeActions",
+  --     "codeActionResolve",
+  --     "documentHighlight",
+  --     "inlayHints",
+  --     "completion",
+  --     "codeLens",
+  --   },
+  -- },
 })
