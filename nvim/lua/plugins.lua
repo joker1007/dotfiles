3a083c7cc32541a2dd3bea9beed2b35c97520252
joker1007 <kakyoin.hierophant@gmail.com>
2025-03-05 22:06:42 +0900

    comment out image.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1350,42 +1350,42 @@ require("lazy").setup({
       })
     end,
   },
-  {
-    "3rd/image.nvim",
-    build = "luarocks --local install magick",
-    config = function()
-      if not vim.g.neovide and not vim.g.GuiLoaded then
-        require("image").setup({
-          backend = "ueberzug",
-          integrations = {
-            markdown = {
-              enabled = true,
-              clear_in_insert_mode = true,
-              download_remote_images = true,
-              only_render_image_at_cursor = true,
-              filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-            },
-            neorg = {
-              enabled = true,
-              clear_in_insert_mode = false,
-              download_remote_images = true,
-              only_render_image_at_cursor = false,
-              filetypes = { "norg" },
-            },
-          },
-          max_width = 480,
-          max_height = 160,
-          max_width_window_percentage = nil,
-          max_height_window_percentage = nil,
-          window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-          window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-          editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
-          tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-          hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
-        })
-      end
-    end,
-  },
+  -- {
+  --   "3rd/image.nvim",
+  --   build = "luarocks --local install magick",
+  --   config = function()
+  --     if not vim.g.neovide and not vim.g.GuiLoaded then
+  --       require("image").setup({
+  --         backend = "ueberzug",
+  --         integrations = {
+  --           markdown = {
+  --             enabled = true,
+  --             clear_in_insert_mode = true,
+  --             download_remote_images = true,
+  --             only_render_image_at_cursor = true,
+  --             filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+  --           },
+  --           neorg = {
+  --             enabled = true,
+  --             clear_in_insert_mode = false,
+  --             download_remote_images = true,
+  --             only_render_image_at_cursor = false,
+  --             filetypes = { "norg" },
+  --           },
+  --         },
+  --         max_width = 480,
+  --         max_height = 160,
+  --         max_width_window_percentage = nil,
+  --         max_height_window_percentage = nil,
+  --         window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+  --         window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+  --         editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+  --         tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+  --         hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
+  --       })
+  --     end
+  --   end,
+  -- },
   {
     "mikavilpas/yazi.nvim",
     event = "VeryLazy",
