cce3db7bedac16992379f9d4371261f4c185c7c9
joker1007 <kakyoin.hierophant@gmail.com>
2025-06-16 04:29:39 +0900

    delete nvim_solargraph.lua

--- nvim/lua/configs/enable_solargraph.lua
+++
@@ -1,8 +0,0 @@
-local lsp_common = require "lsp_common"
-local on_attach = lsp_common.on_attach
-local lspconfig = require "lspconfig"
-
-lspconfig.solargraph.setup({
-  capabilities = lsp_common.make_lsp_capabilities(),
-  on_attach = on_attach,
-})
