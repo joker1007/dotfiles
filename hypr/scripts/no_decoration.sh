b7a6c98a4d03970c80dc23ae48bf4db89d549bd7
joker1007 <kakyoin.hierophant@gmail.com>
2025-06-16 04:30:23 +0900

    update hypr/no_decoration.sh

--- hypr/scripts/no_decoration.sh
+++ hypr/scripts/no_decoration.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==2{print $2}')
-if [ "$HYPRGAMEMODE" = 1 ] ; then
+if [ "$HYPRGAMEMODE" = "true" ] ; then
     hyprctl --batch "\
         keyword animations:enabled 0;\
-        keyword decoration:drop_shadow 0;\
         keyword decoration:blur:enabled 0;\
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
