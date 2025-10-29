7c02505d1791fb527b93d23c7c7c3613899ea25a
joker1007 <kakyoin.hierophant@gmail.com>
2025-10-29 17:03:58 +0900

    update luasnip plugin definition

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -844,6 +844,7 @@ require("lazy").setup({
     dependencies = {
       "rafamadriz/friendly-snippets",
     },
+    build = "make install_jsregexp",
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load()
       vim.cmd [[
